2ca3bcce63184f109c91d27f3dba083e43de5e0d
Gnxela <qawswedfrgtt@gmail.com>
2019-09-27 13:56:47 +0100

    Added basic bot class that can log into Discord.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import javax.security.auth.login.LoginException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	public static void main(String[] args) throws LoginException, InterruptedException {
+		new Bot("NjI3MTIxNjEyMTMwNjE1Mjk2.XY4CwA.nd5MFKGE6_SV2Mkb8Or40TQ5s3M").init();
 	}
 
 }
